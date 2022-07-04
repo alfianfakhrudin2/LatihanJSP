25ca7fabe7096cb2c2bfbe9b2a8b07ac8678c448
Alfian Fakhrudin <alfianfakhrudin11@gmail.com>
2022-07-05 01:22:32 +0700

    menambahkan function date
    menambahkan function untuk mengubah yy/mm/dd menjadi dd/mm/yy

--- Helper/StringHelper.java
+++ Helper/StringHelper.java
@@ -13,4 +13,10 @@ public class StringHelper {
     public static String parseLikeQuery(String query) {
         return "%" + query + "%";
     }
+    public static String modifyDateIntoDatepicker(String expired) {
+        String[] splitted = expired.split("-");
+        expired = splitted[1] + "/" + splitted[2] + "/" +  splitted[0] ;
+        
+        return expired;
+    }
 }
